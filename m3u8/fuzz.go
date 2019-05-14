04e8ba8f44b11d6e33e4b650dde0527ef8f24c45
gy741 <gy741.kim@gmail.com>
2019-05-14 15:05:00 +0900

    Update fuzz.go

--- m3u8/fuzz.go
+++ m3u8/fuzz.go
@@ -1,11 +1,12 @@
 package m3u8
 
 import "bytes"
+import "bufio"
 
 func Fuzz(data []byte) int {
-
+        a := bytes.NewReader(data)
         p := NewMasterPlaylist()
-        err := p.DecodeFrom(bytes.NewReader(data), false)
+        err := p.DecodeFrom(bufio.NewReader(a), false)
         if err != nil {
                 return 0
         }
